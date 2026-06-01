de7fc2d6e4103955d4c4061c681dda51132ffeee
Francis Narange <francisnarange15@gmail.com>
2026-06-01 11:37:05 +0530

    ..

--- src/main/java/Self_Prac/FindMissingNumber.java
+++ src/main/java/Self_Prac/FindMissingNumber.java
@@ -2,15 +2,44 @@ package Self_Prac;
 
 public class FindMissingNumber {
 
-//    public static int findMissingNumber(int[] arr){
-//
-//        if(arr.length<2 || arr[0]<arr.length){
-//            return 1;
-//        }
-//
-//    }
+    public static int findMissingNumber(int[] arr){
+
+        int n = arr.length + 1;
+
+        // Iterate from 1 to n and check
+        // if the current number is present
+        for (int i = 1; i <= n; i++) {
+            boolean found = false;
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[j] == i) {
+                    found = true;
+                    break;
+                }
+            }
+
+            // If the current number is not present
+            if (!found)
+                return i;
+        }
+        return -1;
+    }
+
+    public static int missingNoXor(int[] arr){
+            int n = arr.length + 1;
+            int xor = 0;
+
+            for (int i = 1; i <= n; i++)
+            {xor ^= i;}       // XOR 1 to n
+            for (int num : arr) xor ^= num;               // XOR with array
+
+            return xor; // same numbers cancel out, missing remains
+        }
+
 
     public static void main(String[] args) {
 
+        int[] arr = {8,7,6,5,4,3,1};
+        //System.out.println(findMissingNumber(arr));
+        System.out.println(missingNoXor(arr));
     }
 }
